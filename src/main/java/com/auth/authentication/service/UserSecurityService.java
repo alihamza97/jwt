3edf5723c39c8c863b99918fb04cc0afb428e1ed
Ali Hamza <alihamza@Alis-MacBook-Pro.local>
2025-10-01 17:34:29 +0200

    added all the necessary classes for the basic authentication with jwt

---
+++ src/main/java/com/auth/authentication/service/UserSecurityService.java
@@ -0,0 +1,31 @@
+package com.auth.authentication.service;
+
+import com.auth.authentication.model.User;
+import com.auth.authentication.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class UserSecurityService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserSecurityService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities(Collections.emptyList())
+                .build();
+    }
+}
